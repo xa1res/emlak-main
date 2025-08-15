8e1db687e690d0f78b653eebeae19049e0cb797a
xaires <metexairesdugan@gmail.com>
2025-08-15 15:41:17 +0300

    asd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://penta-backend.onrender.com/api', // prod’da aynı domain altından /api
-  serverApiUrl: 'https://penta-backend.onrender.com/api'  // prod SSR’de kullanacaksan ayarla
+  apiUrl: 'https://penta-backend.onrender.com/api', // Render domainin
+  serverApiUrl: 'https://penta-backend.onrender.com/api' // SSR kullanıyorsan da aynı
 };
+  
